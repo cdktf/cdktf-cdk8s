eb30c69140027a762f9aeb01d3d0012c0817c110
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-01-14 11:23:56 +0100

    chore: remove explicit project base class version constraint

--- .projenrc.js
+++ .projenrc.js
@@ -7,7 +7,7 @@ const project = new CDKTFConstruct({
   repositoryUrl: "https://github.com/DanielMSchmidt/cdktf-cdk8s.git",
   bundledDeps: ["yaml@1.10.2"],
   peerDeps: ["@cdktf/provider-kubernetes@>=0.5.129", "cdk8s@>=2.1.6"],
-  devDeps: ["@dschmidt/cdktf-construct-base@0.0.6", "cdk8s-cli@1.0.74"],
+  devDeps: ["@dschmidt/cdktf-construct-base", "cdk8s-cli"],
   description:
     "A compatability layer for using cdk8s constructs within Terraform CDK." /* The description is just a string that helps people understand the purpose of the package. */,
   // devDeps: [],             /* Build dependencies for this module. */
