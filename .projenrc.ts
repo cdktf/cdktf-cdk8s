14141cdf6cd5eae97c84868bc4d7b5b894f1cf54
team-tf-cdk <84392119+team-tf-cdk@users.noreply.github.com>
2023-08-24 14:37:36 -0400

    chore!: upgrade to cdktf 0.18.0 (#202)
    This PR initiates the upgrade of CDKTF from version `0.17.0` to version
    `0.18.0`.
    Unfortunately, not everything can be automated, and the following steps
    need to be completed manually:
     
    - [x] Update `@cdktf/provider-kubernetes` to a version compatible with
    `cdktf@0.18.0`
    [here](https://github.com/cdktf/cdktf-cdk8s/blob/4d23de9f0dadddb922bf1c44079f08856491f11b/.projenrc.ts#L67).
    Look up the version
    [here](https://github.com/cdktf/cdktf-provider-kubernetes/releases/).
    - [x] Run `npx projen`
     
    Please checkout this PR, complete the above steps, push the changes to
    this branch, and then mark this PR as ready for review to complete the
    upgrade. Thanks!
    
    ---------
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>
    Co-authored-by: Nara Kasbergen <nara.kasbergen@gmail.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -43,7 +43,7 @@ const project = new ConstructLibraryCdktf({
     ignorePatterns: ["**/node_modules/**", "**/test/imports/**"],
   },
   docgen: false,
-  cdktfVersion: "0.17.0",
+  cdktfVersion: "0.18.0",
   publishToPypi: {
     distName: name,
     module: name.replace(/-/g, "_"),
@@ -67,9 +67,9 @@ new UpgradeCDKTF(project);
 
 project.addPeerDeps(
   "constructs@^10.0.25",
-  "@cdktf/provider-kubernetes@>=7.0.0",
+  "@cdktf/provider-kubernetes@>=9.0.0",
   "cdk8s@>=2.1.6",
-  "cdktf@>=0.17.0"
+  "cdktf@>=0.18.0"
 );
 
 project.addDevDeps("cdk8s-cli@>=2.0", "ts-node@10.9.1");
