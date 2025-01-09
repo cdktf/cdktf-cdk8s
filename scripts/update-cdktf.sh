b266254292da0af24f18490dcadad0b104826886
Nara Kasbergen Kwon <855115+xiehan@users.noreply.github.com>
2025-01-09 19:24:33 +0100

    chore(ci): update required version of constructs when cdktf is upgraded (#311)

--- scripts/update-cdktf.sh
+++ scripts/update-cdktf.sh
@@ -6,17 +6,24 @@ set -ex
 
 PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE:-$0}")/.." && pwd)
 CDKTF_VERSION=$1
+CONSTRUCTS_VERSION=$2
 
 if [ -z "$CDKTF_VERSION" ]; then
-  echo "Usage: $0 <cdktf-version>"
+  echo "Usage: $0 <cdktf-version> <constructs-version>"
+  exit 1
+fi
+if [ -z "$CONSTRUCTS_VERSION" ]; then
+  echo "Usage: $0 <cdktf-version> <constructs-version>"
   exit 1
 fi
 
-echo "Updating to cdktf version $CDKTF_VERSION"
+echo "Updating to cdktf version $CDKTF_VERSION and constructs version $CONSTRUCTS_VERSION"
 yarn
 sed -i "s/cdktfVersion: \".*\",/cdktfVersion: \"$CDKTF_VERSION\",/" "$PROJECT_ROOT/.projenrc.ts"
 sed -i "s/\"cdktf@>=.*\"/\"cdktf@>=$CDKTF_VERSION\"/" "$PROJECT_ROOT/.projenrc.ts"
+sed -i "s/constructsVersion = \".*\";/constructsVersion = \"$CONSTRUCTS_VERSION\";/" "$PROJECT_ROOT/.projenrc.ts"
 CI=0 npx projen
 
 echo "Updating README"
 sed -i 's/`cdktf` >= .*/`cdktf` >= '"$CDKTF_VERSION"'/' "$PROJECT_ROOT/README.md"
+sed -i 's/`constructs` >= .*/`constructs` >= '"$CONSTRUCTS_VERSION"'/' "$PROJECT_ROOT/README.md"
